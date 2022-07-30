1e951648a2c8299be20cfbd3df1f66254ffcda5b
Jonathan St-Michel <j.stmichel@icloud.com>
2022-07-30 16:00:55 -0400

    #4 feat: Create identity api structure
    - Add health controller with simple get
    - Update editor configuration rules.
    - Add .gitignore file

---
+++ src/FamilyQuest.Identity.Api/Program.cs
@@ -0,0 +1,33 @@
+// Licensed under the MIT license.
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.LowercaseQueryStrings = true;
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
